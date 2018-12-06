26beee41ebf5007b601fac10d520d317f899d8de
郝维格 <979955176@qq.com>
2018-12-06 16:39:57 +0800

    加了一点注释作为笔记

--- src/main/java/com/spring/_06scope/CreationBeanFactory.java
+++ src/main/java/com/spring/_06scope/CreationBeanFactory.java
@@ -1,10 +1,18 @@
 package com.spring._06scope;
 
 public class CreationBeanFactory {
+    /**
+     * 通过静态方法创建
+     * @return
+     */
     public static CreationBean getCreation1(){
         return new CreationBean();
     }
 
+    /**
+     * 通过普通方法创建
+     * @return
+     */
     public CreationBean getCreation2(){
         return new CreationBean();
     }
--- src/main/java/com/spring/_06scope/CreationFactoryBean.java
+++ src/main/java/com/spring/_06scope/CreationFactoryBean.java
@@ -3,14 +3,15 @@ package com.spring._06scope;
 import org.springframework.beans.factory.FactoryBean;
 
 public class CreationFactoryBean implements FactoryBean {
+    //创建CreationBean对象
     public Object getObject() throws Exception {
         return new CreationBean();
     }
-
+    //返回CreationBean类型
     public Class<?> getObjectType() {
         return CreationBean.class;
     }
-
+    //是否单例
     public boolean isSingleton() {
         return true;
     }
